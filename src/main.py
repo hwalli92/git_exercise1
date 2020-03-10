ded0c777b928f998a4a7ce05a832d9d0be111d92
hwalli92 <62026003+hwalli92@users.noreply.github.com>
2020-03-10 17:08:37 +0100

    updated main.py

---
+++ src/main.py
@@ -0,0 +1,10 @@
+first_number = 1+1+5
+print(first_number)
+second_number = 105+10
+print(second_number)
+third_number = 20+340
+print (third_number)
+
+total = first_number + second_number + third_number
+
+print(total)
